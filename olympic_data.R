4318bfcc780aaa9b4f314d6259b1129a758f61d7
Keishiro Miwa <keishiro-sils@akane.waseda.jp>
2018-02-22 18:49:06 -0800

    made keys.R

--- olympic_data.R
+++ olympic_data.R
@@ -2,9 +2,10 @@ library(jsonlite)
 library(httr)
 library(RCurl)
 
+source("keys.R")
 OlympicsData <- function(location){
   #query.params <- list(competitor = 5719, api_key = mdvfq3f2rrukpzjtcxse39ba)
-  response <- GET("http://api.sportradar.us/curling-t1/en/competitors/sr:competitor:5707/profile.json?api_key=mdvfq3f2rrukpzjtcxse39ba")
+  response <- GET(key = api.key)
   body <- content(response, "text")
   results <- fromJSON(body)
   players <- flatten(results$players)
