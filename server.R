222923cf41222d6c4ca56ec5beec59c5ffad7da5
Nick Hytrek <nshytrek@gmail.com>
2018-03-05 00:03:10 -0800

    edits to fix colorbar text not being fully visible

--- server.R
+++ server.R
@@ -66,7 +66,7 @@ shinyServer(function(input, output) {
         z = df$Medals, color = df$Medals, colors = "Blues",
         text = df$Country, locations = df$Code, marker = list(line = list(color = toRGB("black"), width = 0.5))
       ) %>%
-      colorbar(title = "Number of Medals") %>%
+      colorbar(title = "Medals") %>%
       layout(
         title = "Total Olympic Medals Won for Each Country",
         geo = list(showframe = FALSE,
