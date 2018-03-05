2d884a40946c0dd7ab52750d8716c4ae0ad0dea9
Nick Hytrek <nshytrek@gmail.com>
2018-03-05 15:52:14 -0800

    Edits made to make the map more readable

--- ui.R
+++ ui.R
@@ -38,17 +38,18 @@ shinyUI(
 
 #---------------------------Nick----------------------------------------
     tabPanel("Nick", titlePanel("Nick"),
-      sidebarLayout(
-        sidebarPanel( 
-          sliderInput(inputId = "year", label = "Which year are you intersted in?", 
-                      min = 1896, max = 2012, value = 1896, step = 4, 
-                      ticks = TRUE, animate = animationOptions(interval = 4000)
-          )
-        ),
-        mainPanel(
-          plotlyOutput("worldmap", width = "1000px")
-        )
-      )
+     verticalLayout(
+       titlePanel("Total Metals Won by Each Country for Every Olympic Year"),
+       plotlyOutput("worldmap"),
+       wellPanel(
+         sliderInput(inputId = "year",
+                     label = "Which year are you intersted in?",
+                     min = 1896, max = 2012, value = 1896, step = 4, 
+                     ticks = TRUE,
+                     animate = animationOptions(interval = 4000)
+         )
+       )
+     )
     ),
 
 #----------------------Shannon-------------------------------------
