9750ba64a6cd0bd715e8266b30649e80d5de64f5
Keishiro Miwa <keishiro-sils@akane.waseda.jp>
2018-03-06 12:20:51 -0800

    ediited to fix the bug

--- ui.R
+++ ui.R
@@ -40,19 +40,17 @@ shinyUI(
 
 #---------------------------Nick----------------------------------------
     tabPanel("World Map", titlePanel(" "),
-     verticalLayout(
-       titlePanel("Total Metals Won by Each Country for Every Olympic Year"),
-       helpText(h5("Notice: "), 
-                p("There is no data for the following years due to global conflicts; 1916, 1940, 1944")
-       wellPanel(
-         sliderInput(inputId = "year",
-                     label = "Which year are you intersted in?",
-                     min = 1896, max = 2012, value = 1896, step = 4, 
-                     ticks = TRUE,
-                     animate = animationOptions(interval = 4000)
-         )
-       )
-     )
+      verticalLayout(
+        titlePanel("Total Metals Won by Each Country for Every Olympic Year"),
+        helpText(h5("Notice: "),
+                 p("There is no data for the following years due to global conflicts; 1916, 1940, 1944")),
+        wellPanel(
+          sliderInput(inputId = "year", label = "Which year are you intersted in?",
+                      min = 1896, max = 2012, value = 1896, step = 4,
+                      ticks = TRUE, animate = animationOptions(interval = 4000)
+          )
+        )
+      )
     ),
 
 #----------------------Shannon-------------------------------------
@@ -69,23 +67,20 @@ shinyUI(
       )
     ),
 
-tabPanel("About Us", titlePanel("The History of the Olympics"),
-
-
-         mainPanel(
-           helpText("The Olympic Games are an international sports festival that began in ancient Greece.
-                    The original Greek games were staged every fourth year for several hundred years, until 
-                    they were abolished in the early Christian era. The revival of the Olympic Games took place 
-                    in 1896, and since then they have been staged every fourth year, except during World War I and
-                    World War II (1916, 1940, 1944). The purpose of our analysis is to find important trends in the
-                    medals won by the Olympians from every country, man and woman from 1896 to 2014. Depending on the
-                    country and their specific rules, women were not able to compete until later years, explaining the
-                    disparity in their display on the third visualization."),
-           helpText("Keishiro Miwa is an International Student from Japan. He's in his second quarter as a Sophmore at Waseda University."),
-           helpText("Shannon Gatta is an Informatics major at the University of Washington. She is a Junior. She is from Dallas, TX."),
-           helpText("Nick Hytrek is a Classics major and an Informatics minor at the University of Washington. He is a Junior. He is from Auburn, WA. ")
-           ))
-
-
-
-))
+    tabPanel("About Us", titlePanel("The History of the Olympics"),
+      mainPanel(
+        helpText("The Olympic Games are an international sports festival that began in ancient Greece.
+                  The original Greek games were staged every fourth year for several hundred years, until 
+                  they were abolished in the early Christian era. The revival of the Olympic Games took place 
+                  in 1896, and since then they have been staged every fourth year, except during World War I and
+                  World War II (1916, 1940, 1944). The purpose of our analysis is to find important trends in the
+                  medals won by the Olympians from every country, man and woman from 1896 to 2014. Depending on the
+                  country and their specific rules, women were not able to compete until later years, explaining the
+                  disparity in their display on the third visualization."),
+        helpText("Keishiro Miwa is an International Student from Japan. He's in his second quarter as a Sophmore at Waseda University."),
+        helpText("Shannon Gatta is an Informatics major at the University of Washington. She is a Junior. She is from Dallas, TX."),
+        helpText("Nick Hytrek is a Classics major and an Informatics minor at the University of Washington. He is a Junior. He is from Auburn, WA. ")
+      )
+    )
+  )
+)
